956243b4592e08c43260cc0074429e4b253df7da
oğuzhan çolak <oguzhan22_@hotmail.com>
2020-11-11 12:01:41 +0300

    Giriş ekranı yapımına başlandı.

---
+++ Otobüs-Bilet Otomasyonu/Otobüs-Bilet Otomasyonu/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Otobüs_Bilet_Otomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kullanıcı Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Şifre:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Giriş Yap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 86);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(252, 200);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox4;
+        public System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Otobüs-Bilet Otomasyonu/Otobüs-Bilet Otomasyonu/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Otobüs_Bilet_Otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form2 f2 = new Form2();
+        SqlConnection baglan = new SqlConnection("Data Source=TOSHIBA-PC\\SQLEXPRESS;Initial Catalog=Otobus-Bılet-Otomasyonu;Integrated Security=True");
+
+        private void verilerigoster()
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("select * from Personel", baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+
+                if ((oku["KullanıcıAdı"].ToString() == "fevzi22") && (oku["Sifre"].ToString() == "gjeo5dk"))
+                {
+                    f2.label1.Text = "Fevzi kardeşim hoşgeldin";
+                }
+                else if ((oku["KullanıcıAdı"].ToString() == "salihkk") && (oku["Sifre"].ToString() == "5d8cm2h"))
+                {
+                    f2.label1.Text = "Salih kardeş hoşgeldin";
+                }
+            }
+            baglan.Close();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            verilerigoster();
+
+            this.Hide();
+
+            f2.Show();
+
+            //Sayısal değer çekseydik: double s1 = Convert.ToDouble(textBox1.Text);
+            //                            int s1 = int.Parse(textBox1.Text);
+            //https://youtu.be/Z-9cV6-m0Mc
+
+        }
+    }
+}
